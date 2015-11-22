f65300e8c959c3213a0d15d1aac9fd5c5b3165cd
sander <sander@groowup.nl>
2015-11-22 17:05:02 +0100

    Initial set up manager, not sure if i've done this correctly though.

--- src/manager/Manager.java
+++ src/manager/Manager.java
@@ -1,4 +1,6 @@
 package manager;
+import java.util.Random;
+
 import storm.bloomfilter.*;
 import storm.bolt.*;
 import storm.rdf.*;
@@ -6,25 +8,47 @@ import storm.spout.*;
 import storm.topology.*;
 
 public class Manager {
+	
+	String query = "";
+	RDFTriple latest = new RDFTriple(" "," "," ");
+	RDFSpout = new RDFSpout();
+	
 	public static void main(String[] args) {
+		Manager manager = new Manager();
+		manager.start("TODO");
 		
 		
 	}
 	
-	void start(String query){
+	void start(String input){
 		//do stuff with the query
-	}
-	
-	void stream(){
-		while(true){
-			//generate RDF triples etc
+		query = input;
+		try {
+			stream();
+		} catch (InterruptedException e) {
+			System.out.println("interruptedException");
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 	
-	
+	void stream() throws InterruptedException{
+		int wait = 0;
+		Random rand = new Random();
+		while(true){
+			wait = rand.nextInt(10000);//wait somewhere in between 0 and 10 seconds
+			Thread.sleep(wait);//emulate randomness when new events occur
+			
+			//generate RDF triples etc
+			//how to use RDFSpout class? Has no return values?
+			//store new RDF triple in latest variable
+		}
+	}	
 	
 	void read(){
-		//read one triple
+		//read triple latest
+		//select what needs to be stored using the query string
+		//store needed info, delete the rest
 	}
 
 }
