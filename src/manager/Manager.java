9f6ea16058e5d86d12e5d70de9fb2bc146a6c8be
sander <sander@groowup.nl>
2015-11-22 14:50:13 +0100

    Can i submit to this?

---
+++ src/manager/Manager.java
@@ -0,0 +1,30 @@
+package manager;
+import storm.bloomfilter.*;
+import storm.bolt.*;
+import storm.rdf.*;
+import storm.spout.*;
+import storm.topology.*;
+
+public class Manager {
+	public static void main(String[] args) {
+		
+		
+	}
+	
+	void start(String query){
+		//do stuff with the query
+	}
+	
+	void stream(){
+		while(true){
+			//generate RDF triples etc
+		}
+	}
+	
+	
+	
+	void read(){
+		//read one triple
+	}
+
+}
