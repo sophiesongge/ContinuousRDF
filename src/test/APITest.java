922cdad0bc41d22491f66946499c09c8cbb50228
sander <sander@groowup.nl>
2016-01-24 00:24:57 +0100

    Tried to create a class with tests, found out there are incorrect results.

---
+++ src/test/APITest.java
@@ -0,0 +1,22 @@
+package test;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class APITest  extends TestCase{
+	/*
+	 * Test results for TopologyWithThreeBF:
+	 * 
+	 * input: (Ph.D,any,any)
+	 * output: [Sophie, Fabrice, Lea, Frederic, Justine]
+	 * should be: [Sophie, Bob, Johne, Laura, Sergie, Yuki, Yume, Linda, Sabrina, Justine, Fabrice, Frederic]
+	 * 
+	 * Input: (Master,any,any)
+	 * output: [Sophie, Justine, Fabrice, Lea, Frederic]
+	 * should be: [Lea]
+	 */
+}
