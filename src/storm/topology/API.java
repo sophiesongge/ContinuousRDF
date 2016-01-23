5532fd6e41682e796e6b7d7b29ade1195676df82
sander <sander@groowup.nl>
2016-01-24 00:24:04 +0100

    any --> ANY

--- src/storm/topology/API.java
+++ src/storm/topology/API.java
@@ -1,17 +1,32 @@
 package storm.topology;
 
+import backtype.storm.Config;
+import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.tuple.Fields;
+import storm.bolt.BoltBuilderWithThreeBF;
+import storm.bolt.BoltProberWithThreeBF;
+import storm.rdf.Query;
+import storm.spout.RDFSpoutWithThreeBF;
+
 public class API {
 	
 	public String[] singleVarJoin(String var){		
-		return multiVarJoin(var, "any", "any");
+		return multiVarJoin(var, "ANY", "ANY");
 	}
 	
 	public String[] doubleVarJoin(String var1,String var2){		
-		return multiVarJoin(var1, var2, "any");		
+		return multiVarJoin(var1, var2, "ANY");		
 	}
 	
 	public String[] multiVarJoin(String var1, String var2, String var3){
 		String[] returnval = new String[]{};
+		Query query = new Query(var1,var2,var3);		
+		Config config = new Config();
+		
+		TopologyBuilder builder = new TopologyBuilder();
+		builder.setSpout("spout_getdata", new RDFSpoutWithThreeBF(),3);
+		builder.setBolt("bolt_builder", new BoltBuilderWithThreeBF(),1).fieldsGrouping("spout_getdata", new Fields("Predicate"));
+		builder.setBolt("bolt_prober", new BoltProberWithThreeBF(),1).shuffleGrouping("bolt_builder");
 		//todo
 		return returnval;
 	}
