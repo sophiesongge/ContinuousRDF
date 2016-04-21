96b83acaff814ae9771ff86a3d131bf5f0271c1f
sander <sander@groowup.nl>
2016-04-21 15:53:58 +0200

    Added test benchmark file

---
+++ src/storm/benchmark/RDFTopology.java
@@ -0,0 +1,19 @@
+package storm.benchmark;
+
+
+
+
+public class RDFTopology{
+	
+	
+	public static void main(String[] args) throws Exception{
+
+		String filePath="./data/University_combined.daml";
+		System.out.println("Test");
+	}
+	
+	
+	
+	
+
+}
