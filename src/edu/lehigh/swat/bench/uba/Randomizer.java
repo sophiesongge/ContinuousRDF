31b10eed7f183a9d90ebfe8f19909ee9804a8f3b
sander <sander@groowup.nl>
2016-06-01 13:48:17 +0200

    Added randomizer class, still far from done

---
+++ src/edu/lehigh/swat/bench/uba/Randomizer.java
@@ -0,0 +1,16 @@
+package edu.lehigh.swat.bench.uba;
+
+public class Randomizer {
+
+	public void randomize(){
+		String folder = "E:\\Dropbox\\Sander\\College\\RDFProject\\git\\ContinuousRDF\\data";
+		System.out.println("Files to randomize are in folder " + folder);
+		
+	}
+	
+	public static void main(String[] args) {
+		Randomizer randomizer = new Randomizer();
+		randomizer.randomize();
+	}
+
+}
